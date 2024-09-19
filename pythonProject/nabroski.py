3913c53e7d1630593e2bcf9261d3a709b88ec3f8
kahurinmihail <113550658+kahurinmihail@users.noreply.github.com>
2024-09-19 23:55:26 +0300

    DZ

---
+++ pythonProject/nabroski.py
@@ -0,0 +1,13 @@
+#print ("Hello mazafaka2 ",5,7,9,".", sep="|")
+# комментарий
+#print ("текст после комментария")
+#print ('Result :',5+5)
+
+#input ("ВВЕДИ СВОЙ ВОЗРАСТ Ссынок: ")
+
+NUM1 = str(input ("Любой символ: "))
+print ("Результат", NUM1)
+print ("Результат", NUM1,NUM1)
+print ("Результат", NUM1,NUM1,NUM1)
+print ("Результат", NUM1,NUM1,NUM1,NUM1)
+print ("Результат", NUM1,NUM1,NUM1,NUM1,NUM1)
